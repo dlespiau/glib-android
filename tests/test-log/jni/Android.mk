c1d9912d39bfff641a3370bcff0c03375d2ec689
Damien Lespiau <damien.lespiau@intel.com>
2011-02-19 16:15:49 +0000

    tests: Add test-log that uses the g_log integration
    GLib's log domains are becoming the "tags" of android logs and a logical
    mapping is done between the GLib's log levels and the Android ones.
    
    test-log has G_LOG_DOMAIN defined to "TestLog" and here is the output of
    a g_message ("command: LOST_FOCUS"); in adb logcat:
    
    I/TestLog ( 1298): command: LOST_FOCUS

---
+++ tests/test-log/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := test-log
+LOCAL_SRC_FILES := main.c
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_STATIC_LIBRARIES := android_native_app_glue glib-android glib iconv
+LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS := -DG_LOG_DOMAIN=\"TestLog\"
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
+$(call import-module,glib)
