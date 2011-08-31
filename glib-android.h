8be50422fac18fa35cf16682ec1bb8a485456485
Damien Lespiau <damien.lespiau@intel.com>
2011-08-31 18:52:50 +0100

    misc: Add dual license Apache 2.0 / LGPL v2.1+

--- glib-android.h
+++ glib-android.h
@@ -1,6 +1,25 @@
 /*
  * Copyright (C) 2011 Intel Corporation
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +32,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * Authors:
+ *  Damien Lespiau <damien.lespiau@gmail.com>
  */
 
 #ifndef __GLIB_ANDROID_H__
